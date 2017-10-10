32a5ab02a99ed93d5f767f5bf62724329c23737b
zekeriyasari <zekeriya.sari.deu@gmail.com>
2017-10-10 18:27:15 +0300

    Started random_algorithm. random_worker does not work properly.

--- src/ifs.jl
+++ src/ifs.jl
@@ -37,10 +37,6 @@ end
 # Constructor rule out explicit type declaration.
 RealSpace(set::Type{Vector{T}}, dim::Integer) where {T<:Real} =
     RealSpace{T}(set::Type{Vector{T}}, dim::Integer)
-# Add method to `Base.show` to display `RealSpace` types.
-# Base.show(io::IO, space::RealSpace) = print(io, "RealSpace\n
-#                                                  set: $(space.set)\n
-#                                                  dim: $(space.dim)")
 
 
 """
@@ -62,13 +58,6 @@ struct Contraction <: Transformation
         end
     end
 end
-# Add method to `Base.show` to display `Contraction` types.
-# Base.show(io::IO, contraction::Contraction) =
-#     print(io, "Contraction\n
-#                rule: $(contraction.rule)\n
-#                domain: $(contraction.domain)\n
-#                range: $(contraction.range)\n
-#                contractivity: $(contraction.contractivity)")
 
 
 """
@@ -99,10 +88,6 @@ end
 # Constructor to convert row arrays to column arrays
 IFS(contractions::Array{Contraction, 2}) =
     IFS(reshape(contractions, length(contractions), ))
-# # Add method `Base.show` to display `IFS` types.
-# Base.show(io::IO, ifs::IFS) = print(io, "IFS\n
-#                                          contractions: $(ifs.contractions)\n
-#                                          contractivity: $(ifs.contractivity)")
 
 
 """
@@ -154,7 +139,6 @@ function deterministic_algorithm(ifs::IFS;
     if monitor != nothing
         channel = Channel(num_track_points)  # Construct data channel
         @schedule monitor(channel)  # Launch the remote monitor.
-
         # Check number of update points.
         if num_update == nothing
             num_update = Int(floor(num_steps / 5))
@@ -172,13 +156,12 @@ function deterministic_algorithm(ifs::IFS;
             func = ifs.contractions[j].rule
             if multi_proc && num_procs > 2
                 # Distribute the set to available processors.
-                # TODO: Check if chunk_size is meaningfull.
                 chunk_size = Int(floor(num_points / num_procs))
                 processes = Vector{Future}(num_procs)
                 for k = 1 : num_procs
                     chunk = set[:, 1 + (k - 1) * chunk_size : k * chunk_size]
                     processes[k] = @spawn deterministic_worker(func, chunk)
-                end  # End of for
+                end
                 # Collect the results from the processors.
                 temp = zeros(size(set))
                 for k = 1 : num_procs
@@ -189,6 +172,10 @@ function deterministic_algorithm(ifs::IFS;
             end
             next_set[:, 1 + (j - 1) * num_points : j * num_points] = temp
         end  # End of for.
+
+        # if size(next_set)[2] > num_track_points
+        #     next_set = next_set[1 : Int(ceil(size(next_set)[2] / num_track_points)) : end]
+        # end
 
         # Moitor the points
         if monitor != nothing && mod(i, num_update) == 0
@@ -205,5 +192,96 @@ function deterministic_algorithm(ifs::IFS;
 end  # End of deterministic_algorithm function
 
 
+# FIXME: num_iter does not passed correctly so data size is not correct. 
+function random_worker(funcs_probs::Tuple{Vector{Contraction}, Vector{Float64}},
+                       initial::Array{Float64, 2},
+                       num_iter::Int64;
+                       discard::Bool=false)
+    funcs, probs = funcs_probs
+    data = zeros(size(initial)[1], num_iter)
+    data[:, 1] = initial
+    for i = 1 : num_iter - 1
+        func = sample(funcs, Weights(probs))    # Choose a random function `func`
+        data[:, i + 1] = func.rule(data[:, i])  # Apply `func` to the data.
+    end
+    println("Data: $data")
+    println("Data Size: $(size(data))")
+    if discard
+        return data[:, end]
+    end
+    return data
+end
+
+
+function random_algorithm(ifs::IFS;
+                          initial::Union{Void, Vector{<:Real}}=nothing,
+                          num_steps::Int=2^10,
+                          probabilities::Union{Vector{<:Real}, Void}=nothing,
+                          num_update::Union{Void, Int}=nothing,
+                          monitor::Union{Void, Function}=nothing,
+                          multi_proc::Bool=true,
+                          num_transients::Int=2^7)
+    # Check initial set.
+    domain_dim = ifs.contractions[1].domain.dim
+    if initial == nothing
+        initial = rand(domain_dim, 1)
+    else
+        if size(initial)[1] != domain_dim
+            throw(DimensionMismatch("Initial does not match ifs domain dimension."))
+        end
+    end
+
+    # Check probabilities
+    if probabilities != nothing
+        sum(probabilities) == 1 || error("Sum of probabilities must be one")
+        length(probabilities) == length(ifs.contractions) || error("Length of probabilities does not match ifs.")
+    else
+        probabilities = ifs.probabilities
+    end
+
+    # If monitor is provided, construct a data channel
+    if monitor != nothing
+        channel = Channel(num_steps)  # Construct data channel
+        @schedule monitor(channel)  # Launch the remote monitor.
+        # Check number of update points.
+        if num_update == nothing
+            num_update = Int(floor(num_steps / 5))
+        end
+    end
+
+    # Transient iterations
+    initial = random_worker((ifs.contractions, probabilities), initial, num_transients, discard=true)
+    println("Initial $initial")
+
+    # Compute the attractor
+    num_procs = CPU_CORES
+    if multi_proc && num_procs > 2  # Use multiple
+        iters = Int(floor(num_steps / num_procs)) * ones(Int, num_procs)
+        if (num_steps % num_procs) != 0
+            iters = [iters; Int(num_steps % num_procs)]
+            num_procs += 1
+        end
+        println("Iters: $iters")
+        println("num_procs: $num_procs")
+        # Launch the remote processes.
+        processes = Vector{Future}(num_procs)
+        for i = 1 : num_procs
+            processes[i] = @spawn random_worker((ifs.contractions, probabilities), initial, iters[i], discard=false)
+        end
+        # Collect the results from the remote processes.
+        set = zeros(size(initial)[1], 1)
+        for i = 1 : num_procs
+            value = fetch(processes[i])
+            println("Set: $set, value: $value")
+            set = [set value]
+        end
+        set = set[:, 2:end]
+    else  # Use single processing
+        set = random_worker((ifs.contractions, probabilities), initial, num_steps, discard=false)
+    end  # End of if-else
+
+    return set
+
+end  # End of deterministic_algorithm function
 
 end # End of IteratedFunctionSystem
